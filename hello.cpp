eb825960d513481aa48df184bc8f7bb855171fa5
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2022-10-04 18:02:39 +0530

    added ip/op

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,9 @@ int main()
     cout<<"Hello, World"<<endl;
    cout<<"Made a change"<<endl;
     cout<<"Is gonna conflict"<<endl;
+    int n;
+    cout<<"enter your number";
+    cin>>n;
+    cout<<"your number is"<<n<<endl;
     //comment 
 }
